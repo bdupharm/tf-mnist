e79927e363fe217c9ff83a8c5eebedf6e222f51b
Bo Du <bdu@truveris.com>
2016-04-16 03:32:18 -0400

    This is definitely wrong.

--- mnist.py
+++ mnist.py
@@ -16,13 +16,28 @@ with tf.Graph().as_default():
     cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
     train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
+    with tf.name_scope("summaries"):
+        weight_summary = tf.scalar_summary("weight/mean", tf.reduce_mean(W))
+        bias_summary = tf.scalar_summary("bias/mean", tf.reduce_mean(b))
+        softmax_summary = tf.scalar_summary("softmax/mean", tf.reduce_mean(y))
+        loss_summary = tf.scalar_summary("loss/mean", tf.reduce_mean(cross_entropy))
+
+    summary_operation = tf.merge_all_summaries()
+
     sess = tf.Session()
     sess.run(tf.initialize_all_variables())
+    summary_writer = tf.train.SummaryWriter("./summaries/", sess.graph)
 
     with sess.as_default():
-        for i in range(1000):
+
+        for step in range(1000):
+
             batch = mnist.train.next_batch(50)
             train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+
+            if step % 10 == 0:
+                summaries = sess.run(summary_operation, feed_dict={x: batch[0], y_: batch[1]})
+                summary_writer.add_summary(summaries, step)
 
         correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
