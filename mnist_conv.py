183f894831f36022eb854d764bd59b463145c2cf
Souloist <rich.shen@nyu.edu>
2016-06-06 01:25:25 -0400

    Fix

--- mnist_conv.py
+++ mnist_conv.py
@@ -5,25 +5,23 @@ import time
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-import util
+import utils
 
 
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 
 with tf.Graph().as_default():
+    # Inputs
     x = tf.placeholder(tf.float32, shape=[None, 784], name="image_inputs")
     y_ = tf.placeholder(tf.float32, shape=[None, 10], name="actual_class")
     """
-    Inputs
-
-    x : 28px by 28px images converted into a [Batch Size * 784] matrix
-    y: [Batch Size * 10] matrix of one hot encodings representing the actual class of the image
+    x : 28px by 28px images converted into a [(Batch Size * 28^2) x 1] column vector
+    y : [Batch Size * 10] matrix of one-hot encodings representing the actual class of the image
        (ie. [ 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 ] where the index of 1 is the class)
-
     """
     with tf.name_scope("conv1"):
         # We arbitrarily choose to use 32 filters (Output Depth) to learn 32 features for each 5x5 patch
         # TODO: Figure out how each filter learns to look for a different feature.
         # TODO: How to choose a number of filters to use. What if... too many/too few?
-        W_conv1 = util.weight([5, 5, 1, 32], name="Weight_conv1")
-        b_conv1 = util.bias([32], name="bias_conv1")
+        W_conv1 = utils.weight([5, 5, 1, 32], name="Weight_conv1")
+        b_conv1 = utils.bias([32], name="bias_conv1")
